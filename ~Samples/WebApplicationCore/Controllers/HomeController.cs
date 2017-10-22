0df97187dec3e35bfcaff4fe4aa0711b24d209ab
zmjack <jack@zmland.com>
2017-10-22 18:26:09 +0800

    SimpleWebApplicationCore

---
+++ ~Samples/WebApplicationCore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationCore.Models;
+
+namespace WebApplicationCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult angular(string data)
+        {
+            return Content(data);
+        }
+
+        [HttpPost]
+        public IActionResult jquery(string data)
+        {
+            return Content(data);
+        }
+
+    }
+}
